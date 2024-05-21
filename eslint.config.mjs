4adc2448baf2cafdc7899cf63f016fb23ace073a
AlamRazek <ohidul933@gmail.com>
2024-05-21 04:04:02 +0100

    config folder added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,13 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 
   {
     rules: {
